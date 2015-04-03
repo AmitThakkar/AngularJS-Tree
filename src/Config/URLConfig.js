cd7b2058f9397a6833b37d439ac2b102643e2ac9
amitthakkar <d0ntgetHyper>
2015-04-03 08:46:29 +0530

    Now format of all the files having same space 4.

--- src/Config/URLConfig.js
+++ src/Config/URLConfig.js
@@ -2,10 +2,10 @@
  * Created by Amit Thakkar on 24/03/15.
  */
 (function (ng) {
-  var treeServiceApp = ng.module('tree.factory', []);
-  treeServiceApp.factory("URLConfig", [function () {
-    return {
-      tree: "api/tree.json"
-    }
-  }]);
+    var treeServiceApp = ng.module('tree.factory', []);
+    treeServiceApp.factory("URLConfig", [function () {
+        return {
+            tree: "api/tree.json"
+        }
+    }]);
 })(angular);
--- src/Directives/TreeDirectives.js
+++ src/Directives/TreeDirectives.js
@@ -2,59 +2,59 @@
  * Created by Amit Thakkar on 24/03/15.
  */
 (function (ng) {
-  var app = ng.module('tree.directives', []);
-  app.directive('nodeTree', function () {
-    return {
-      template: '<node ng-repeat="node in tree"></node>',
-      replace: true,
-      restrict: 'E',
-      scope: {
-        tree: '=children'
-      }
-    };
-  });
-  app.directive('node', function ($compile) {
-    return {
-      restrict: 'E',
-      replace: true,
-      templateUrl: 'partials/node.html', // HTML for a single node.
-      link: function (scope, element) {
-        /*
-         * Here we are checking that if current node has children then compiling/rendering children.
-         * */
-        if (scope.node && scope.node.children && scope.node.children.length > 0) {
-          var childNode = $compile('<ul class="tree" ng-if="node.visibility"><node-tree children="node.children"></node-tree></ul>')(scope);
-          element.append(childNode);
-        }else{
-          scope.symbol = '-';
-        }
-      },
-      controller: ["$scope", function ($scope) {
-        $scope.symbol = '+';
-        // This function is for just toggle the visibility of children
-        $scope.toggleVisibility = function (node) {
-          if(node.children) {
-            node.visibility = !node.visibility;
-            if ($scope.symbol == '-') {
-              $scope.symbol = '+';
-            } else {
-              $scope.symbol = '-';
+    var app = ng.module('tree.directives', []);
+    app.directive('nodeTree', function () {
+        return {
+            template: '<node ng-repeat="node in tree"></node>',
+            replace: true,
+            restrict: 'E',
+            scope: {
+                tree: '=children'
             }
-          }
         };
-        // Here We are marking check/un-check all the nodes.
-        $scope.checkNode = function (node) {
-          node.checked = !node.checked;
-          function checkChildren(c) {
-            angular.forEach(c.children, function (c) {
-              c.checked = node.checked;
-              checkChildren(c);
-            });
-          }
+    });
+    app.directive('node', function ($compile) {
+        return {
+            restrict: 'E',
+            replace: true,
+            templateUrl: 'partials/node.html', // HTML for a single node.
+            link: function (scope, element) {
+                /*
+                 * Here we are checking that if current node has children then compiling/rendering children.
+                 * */
+                if (scope.node && scope.node.children && scope.node.children.length > 0) {
+                    var childNode = $compile('<ul class="tree" ng-if="node.visibility"><node-tree children="node.children"></node-tree></ul>')(scope);
+                    element.append(childNode);
+                } else {
+                    scope.symbol = '-';
+                }
+            },
+            controller: ["$scope", function ($scope) {
+                $scope.symbol = '+';
+                // This function is for just toggle the visibility of children
+                $scope.toggleVisibility = function (node) {
+                    if (node.children) {
+                        node.visibility = !node.visibility;
+                        if ($scope.symbol == '-') {
+                            $scope.symbol = '+';
+                        } else {
+                            $scope.symbol = '-';
+                        }
+                    }
+                };
+                // Here We are marking check/un-check all the nodes.
+                $scope.checkNode = function (node) {
+                    node.checked = !node.checked;
+                    function checkChildren(c) {
+                        angular.forEach(c.children, function (c) {
+                            c.checked = node.checked;
+                            checkChildren(c);
+                        });
+                    }
 
-          checkChildren(node);
+                    checkChildren(node);
+                };
+            }]
         };
-      }]
-    };
-  });
+    });
 })(angular);
